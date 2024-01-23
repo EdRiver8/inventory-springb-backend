386a69e7ea67ad58a8f48af8553ec9aa42af5f4e
EdRiver <dwein08@gmail.com>
2024-01-23 12:31:23 -0500

    finalizando Category

--- src/main/java/com/company/inventory/dao/ICategoryDao.java
+++ src/main/java/com/company/inventory/dao/ICategoryDao.java
@@ -4,6 +4,8 @@ import com.company.inventory.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICategoryDao extends JpaRepository<Category, Long> {
+import java.util.Optional;
 
+public interface ICategoryDao extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
--- src/main/java/com/company/inventory/service/ICategoryService.java
+++ src/main/java/com/company/inventory/service/ICategoryService.java
@@ -1,9 +1,14 @@
 package com.company.inventory.service;
 
+import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import org.springframework.http.ResponseEntity;
 
 public interface ICategoryService {
     ResponseEntity<CategoryResponseRest> search();
     ResponseEntity<CategoryResponseRest> searchById(Long id);
+    ResponseEntity<CategoryResponseRest> save(Category category);
+    ResponseEntity<CategoryResponseRest> update(Category category, Long id);
+    ResponseEntity<CategoryResponseRest> delete(Long id);
+
 }
